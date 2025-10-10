0101a13ae9775a66149e29fb24349bd958abdd3e
Daniel <daniilkacuk@gmail.com>
2025-10-10 17:57:57 +0200

    Лаба 2. Вариант 5

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -3,42 +3,45 @@
 
 
 int main() {
-  //пункт 1
+  //╨Я╤Г╨╜╨║╤В 1
   short A;
   int i;
-
-  setlocale(LC_ALL, "Russian");
   
   std::cin >> A >> i;
   if (i >= 0 && i < 8) {
     int bit = (A >> i) & 1;
-    std::cout << bit << std::endl;
-  } else {
-    int a, b, c;
-    std::cin >> a >> b >> c;
-    a = abs(a);
-    b = abs(b);
-    c = abs(c);
-    if (a == b || a == c || b == c) {
-      std::cout << "Код 0. Вычисление невозможно" << std::endl;
+    if (bit == 0) {
+      if (A & 1) {
+        A ^= (1 << i) | (1 << 0);
+      }
+      std::cout << A;
     } else {
-      int avg = (a + b + c) / 3;
-      std::cout << avg << std::endl;
+      int a, b, c;
+      std::cin >> a >> b >> c;
+      a = abs(a);
+      b = abs(b);
+      c = abs(c);
+      if (a == b || a == c || b == c) {
+        std::cout << "Code 0. Calculating is impossible" << std::endl;
+      } else {
+        float avg = (a + b + c) / 3;
+        std::cout << avg << std::endl;
+      }
     }
   }
-  
-  // пункт 2
+
+  // ╨Я╤Г╨╜╨║╤В 2
   int N;
   std::cin >> N;
   
   switch(N) {
-    case 102: std::cout << "Республика Башкортостан" << std::endl;break;
-    case 11: std::cout << "Республика Коми" << std::endl;break;
-    case 13: std::cout << "Республика Мордовия" << std::endl;break;
-    case 15: std::cout << "Республика Северная Осетия ? Алания" << std::endl;break;
-    case 116: std::cout << "Республика Татарстан" << std::endl;break;
-    case 716: std::cout << "Республика Татарстан" << std::endl;break;
-    default: std::cout << "Информация временно недоступна" << std::endl;break;
+    case 102: std::cout << "Bashkortostan" << std::endl;break;
+    case 11: std::cout << "Komi" << std::endl;break;
+    case 13: std::cout << "Mordovia" << std::endl;break;
+    case 15: std::cout << "North Ossetia-Alania" << std::endl;break;
+    case 116: std::cout << "Tatarstan" << std::endl;break;
+    case 716: std::cout << "Tatarstan" << std::endl;break;
+    default: std::cout << "Information is temporarily unavailable" << std::endl;break;
   }
 
 
