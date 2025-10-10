ab32584c4b9b4e887cd865563741a183f4fe6c60
Daniel <daniilkacuk@gmail.com>
2025-10-10 16:02:37 +0200

    Лаба 1. Вариант 5

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -5,21 +5,28 @@ using namespace std;
 int main() {
     int a; // значения на действительной оси => int
     int b;
+    setlocale(LC_ALL, "Russian");
 
     cin >> a >> b;
 
-    int ans = (a + b) / 2; 
-    
-    int intV = 32;
-    int minInt = pow(2, 31) * (-1);
-    int maxInt = pow(2, 31) -1;
+    float ans = (float(a) + b) / 2;
+
+    int intV = sizeof(int) * 8;
+    int minInt = pow(2, intV - 1) * (-1);
+    int maxInt = pow(2, intV - 1) - 1;
 
     cout << "Середина интервала: " << ans << "\n\n";
-    
+
     cout << "int занимает " << intV << " бит.\n";
-    cout << "min = " << minInt << "\nmax = " << maxInt << "\n";
-  
-    
+    cout << "min = " << minInt << "\nmax = " << maxInt << "\n\n";
+
+    int floatV = sizeof(double) * 8;
+    double minDouble = - numeric_limits<double>::max();
+    double maxDouble = numeric_limits<double>::max();
+
+    cout << "double занимает " << floatV << " бит.\n";
+    cout << "min = " << minDouble << "\nmax = " << maxDouble << "\n";
+
     return 0;
 
 }
